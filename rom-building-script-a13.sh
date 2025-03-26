648d3ade3a714138c3fad2fe4ea477513a789680
ADITYA NANDAN <86918977+HackerAditya56@users.noreply.github.com>
2025-03-26 13:42:45 +0530

    Create rom-building-script-a13.sh
    Rom building script for a13
    device = yogurt
    rom = Derpfest
    
    you can change the value according to your device and rom

---
+++ rom-building-script-a13.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ccache -c # Cache clear
+
+# Setup build environment
+. build/envsetup.sh # you can change for your rom requrinment
+
+# Configure ccache for faster builds
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 50G  # Increase cache size to 100GB for better reuse
+
+# Configure device
+lunch derp_yogurt-user
+
+mka derp -j$(($(nproc --all)*2))  # Use double the number of CPU cores for parallel jobs
+
+# this script is just an example for device = yogurt and rom = derp you can change accroiding to your rom dev guide
